d1dea041e2fb15872ec4feedff5ba801520d708a
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-03-29 18:13:28 +0300

    leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if the year is divisible by four
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
