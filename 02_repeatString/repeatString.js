e3ed38e44aaac5eeeda66ceb9a52ab7433887e3f
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-03-29 13:16:33 +0300

    helloWorld and repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(s, t) {
+    let final = ''
+    for(var i = 0; i < t; i++){
+        final = final.concat(s);
+    }
+    return final;
 };
 
 // Do not edit below this line
