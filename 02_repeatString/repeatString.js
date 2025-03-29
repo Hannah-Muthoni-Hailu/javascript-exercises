7c04ecabe29df2b8d5911fb230f415650915ab7f
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-03-29 13:23:34 +0300

    reverseString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(s, t) {
+    if (t < 0){
+        return 'ERROR';
+    }
+
     let final = ''
     for(var i = 0; i < t; i++){
         final = final.concat(s);
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let result = '';
 
+    for (var i = s.length - 1; i >= 0; i--) {
+        result = result.concat(s[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
