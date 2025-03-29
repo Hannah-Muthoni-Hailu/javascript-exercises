9782eed2439449fc7438792a7efd3becb7a19c8b
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-03-29 18:09:50 +0300

    removeFromArray and sumAll completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rem) {
+    for (var i = 0; i < arr.length; i++) {
+        for(var i = 0; i < rem.length; i++){
+            if (arr[i] == rem[i]){
+                arr.splice(i, 1);
+                rem.splice(i, 1);
+            }
+        }
+    }
+    console.log(arr)
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (isNaN(a) || isNaN(b) || !Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    let res = 0;
 
+    if (a < b){
+        for (var i = a; i <=b; i++) {
+            res = res + i;
+        }
+    } else if (b < a){
+        for (var i = b; i <=a; i++) {
+            res = res + i;
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
