ef7374fedf6c6ade95e5aa1af228403915c42b07
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-03-31 06:12:29 +0000

    fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    num = Number(val)
+    
+    if (num == 0) return 0;
+    else if (num == 1) return 1;
+    else if (num == 2) return 1;
+    else if (num < 0) return "OOPS";
 
+    let arr = [1, 1];
+
+    for (var i = 2; i < num; i++) {
+        arr.push(arr[i-1]+arr[i-2])
+    }
+
+    return arr[num-1]
 };
 
 // Do not edit below this line
