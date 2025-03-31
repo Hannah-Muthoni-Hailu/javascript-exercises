33f8543896a4c38ff285bc19487fd934b3c558e2
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-03-31 06:22:38 +0000

    palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.replace(/[\.,?!]/g, "").toLowerCase().replace(/ /g, "")
+    let arr = s.split("");
+    let rev = [];
+    for (var i = arr.length - 1; i >= 0; i--){
+        rev.push(arr[i]);
+    }
+    let s1 = arr.join("");
+    let s2 = rev.join("")
 
+    return s1 == s2;
 };
 
 // Do not edit below this line
