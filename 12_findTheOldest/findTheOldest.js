88dc1e3c9528cb4f1f9bd4190b4d028f884f4040
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-03-31 06:53:03 +0000

    completed find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let elem = {};
 
+    let res = arr.forEach(element => {
+        let age;
+        if ('yearOfDeath' in element) {
+            age = element['yearOfDeath'] - element['yearOfBirth']
+        } else {
+            age = 2025 - element['yearOfBirth']
+        }
+
+        if (age > oldest) {
+            oldest = age
+            elem = element
+        }
+    });
+    return elem
 };
 
 // Do not edit below this line
